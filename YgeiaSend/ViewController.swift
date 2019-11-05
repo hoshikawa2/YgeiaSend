ab81b282d8095187a81b73940a8380ef82c9021a
Cristiano Hoshikawa <hoshikawa@uol.com.br>
2019-11-05 17:10:04 -0300

    Initial Commit

---
+++ YgeiaSend/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YgeiaSend
+//
+//  Created by Cristiano Hoshikawa on 05/11/19.
+//  Copyright © 2019 Cristiano Hoshikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
